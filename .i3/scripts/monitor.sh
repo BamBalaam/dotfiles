3a557406fb3abf60450eb2d7120fd40e44a8f5c3
André Madeira Cortes <amadeiracortes@gmail.com>
2019-04-17 14:16:39 +0200

    [i3] Better monitor calibration

--- .i3/scripts/monitor.sh
+++ .i3/scripts/monitor.sh
@@ -7,33 +7,49 @@ INTERNAL_OUTPUT="eDP-1"
 
 # if we don't have a file, start at zero
 if [ ! -f "/tmp/monitor_mode.dat" ] ; then
-  monitor_mode="all"
+    monitor_mode="WORK"
 
 # otherwise read the value from the file
 else
-  monitor_mode=`cat /tmp/monitor_mode.dat`
+    monitor_mode=`cat /tmp/monitor_mode.dat`
 fi
 
-if [ $monitor_mode = "all" ]; then
-        monitor_mode="EXTERNAL"
-        xrandr --output $INTERNAL_OUTPUT --off
-	xrandr --output $EXTERNAL_OUTPUT --auto
+if [ $monitor_mode = "WORK" ]; then
+    monitor_mode="EXTERNAL"
+    xrandr --output $INTERNAL_OUTPUT --off
+    xrandr --output $EXTERNAL_OUTPUT --auto
+    xrandr --output $EXTERNAL_OUTPUT_2 --auto --right-of-of $EXTERNAL_OUTPUT
+    xrandr --output $TV_OUTPUT --off
 elif [ $monitor_mode = "EXTERNAL" ]; then
-        monitor_mode="INTERNAL"
-        xrandr --output $INTERNAL_OUTPUT --auto
+    monitor_mode="INTERNAL"
+    xrandr --output $INTERNAL_OUTPUT --auto
 	xrandr --output $EXTERNAL_OUTPUT --off
+    xrandr --output $EXTERNAL_OUTPUT_2 --off
+    xrandr --output $TV_OUTPUT --off
 elif [ $monitor_mode = "INTERNAL" ]; then
-        monitor_mode="CLONES"
-        xrandr --output $INTERNAL_OUTPUT --auto
+    monitor_mode="CLONES"
+    xrandr --output $INTERNAL_OUTPUT --auto
 	xrandr --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
+    xrandr --output $EXTERNAL_OUTPUT_2 --off
+    xrandr --output $TV_OUTPUT --off
 elif [ $monitor_mode = "CLONES" ] ; then
-        monitor_mode="TV"
-        xrandr --output $INTERNAL_OUTPUT --auto
-        xrandr --output $TV_OUTPUT --auto --right-of $INTERNAL_OUTPUT
+    monitor_mode="TV"
+    xrandr --output $INTERNAL_OUTPUT --auto
+    xrandr --output $EXTERNAL_OUTPUT --off
+    xrandr --output $EXTERNAL_OUTPUT_2 --off
+    xrandr --output $TV_OUTPUT --auto --right-of $INTERNAL_OUTPUT
+elif [ $monitor_mode = "TV" ] ; then
+    monitor_mode="WORK-ROTATION"
+    xrandr --output $INTERNAL_OUTPUT --auto
+    xrandr --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT
+    xrandr --output $EXTERNAL_OUTPUT_2 --auto --right-of $EXTERNAL_OUTPUT --rotate left
+    xrandr --output $TV_OUTPUT --off
 else
-        monitor_mode="all"
-        xrandr --output $INTERNAL_OUTPUT --auto
-        xrandr --output $EXTERNAL_OUTPUT_2 --auto --left-of $INTERNAL_OUTPUT
-        xrandr --output $EXTERNAL_OUTPUT --auto --left-of $EXTERNAL_OUTPUT_2
+    monitor_mode="WORK"
+    xrandr --output $INTERNAL_OUTPUT --auto
+    xrandr --output $EXTERNAL_OUTPUT --auto --right-of $INTERNAL_OUTPUT
+    xrandr --output $EXTERNAL_OUTPUT_2 --auto --right-of $EXTERNAL_OUTPUT --rotate normal
+    xrandr --output $TV_OUTPUT --off
 fi
+
 echo "${monitor_mode}" > /tmp/monitor_mode.dat
